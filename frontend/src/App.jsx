243989bbe1b75f087de341f7862402f133e9ca88
JoyMarkarian <jmarkarian@hotmail.fr>
2023-02-03 15:34:49 +0100

    Intégration de la logique d'ajout d'un nouveau projet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import Skills from "./pages/Skills";
 import Projects from "./pages/Projects";
 import OneProject from "./pages/OneProject";
 import ContactForm from "./pages/ContactForm";
+import Admin from "./pages/Admin";
+import NewProject from "./pages/NewProject";
 import Footer from "./components/Footer";
 
 import "./App.css";
@@ -21,6 +23,8 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/projects/:id" element={<OneProject />} />
         <Route path="/contact" element={<ContactForm />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/projects/add" element={<NewProject />} />
       </Routes>
       <Footer />
     </Router>
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 function Footer() {
   return (
     <div className="flex">
       <div className="bg-[#1A1A1A] dark:bg-[#121212] w-1/5" />
       <div className="flex justify-center dark:bg-[#1A1A1A] w-4/5 w-full dark:bg-[#1A1A1A] py-6 text-l">
-        <p className="dark:text-white ">Joy MARKARIAN © Tous droits réservés</p>
+        <p className="dark:text-white ">
+          <Link to="/admin">Joy MARKARIAN</Link> © Tous droits réservés
+        </p>
       </div>
     </div>
   );
