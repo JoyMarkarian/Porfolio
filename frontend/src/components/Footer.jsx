a17c7c168bd2d5b6635dcae23bcd7997a38a3d9b
JoyMarkarian <jmarkarian@hotmail.fr>
2023-02-04 13:03:01 +0100

    Finalisation du footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
+
+import GitHub from "../assets/github2.svg";
+import Linkedin from "../assets/linkedin.svg";
 
 function Footer() {
   return (
     <div className="flex">
-      <div className="bg-[#1A1A1A] dark:bg-[#121212] w-1/5" />
+      <div className="flex justify-center bg-[#1A1A1A] dark:bg-[#121212] w-1/5">
+        <a
+          href="https://github.com/JoyMarkarian"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={GitHub} alt="GitHub" className="my-2 sm:m-2" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/joymarkarian/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={Linkedin} alt="GitHub" className="my-2 sm:m-2" />
+        </a>
+      </div>{" "}
       <div className="flex justify-center dark:bg-[#1A1A1A] w-4/5 w-full dark:bg-[#1A1A1A] py-6 text-l">
         <p className="dark:text-white ">
           <Link to="/admin">Joy MARKARIAN</Link> © Tous droits réservés
