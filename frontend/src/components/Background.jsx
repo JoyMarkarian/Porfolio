9bb8972c2ccdf951b0b1296460ee2749fc36ba62
JoyMarkarian <jmarkarian@hotmail.fr>
2023-02-06 10:24:50 +0100

    Fix background

--- frontend/src/components/Background.jsx
+++ frontend/src/components/Background.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Background() {
   return (
     <div className="flex">
-      <div className="bg-cover flex justify-center items-center w-1/6 py-20 sm:py-24 dark:border-4 border-[#121212]">
+      <div className="bg-cover flex justify-center items-center w-1/6 py-20 sm:py-24">
         <h1 className="-rotate-90 text-[#FFFFFF] text-2xl sm:text-4xl font-extrabold text-center">
           DÉVELOPPEUR WEB
         </h1>
