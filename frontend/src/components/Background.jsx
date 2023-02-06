ef6e21a9f8644e5aa9dc89cc349bcbd6a201d4a3
JoyMarkarian <jmarkarian@hotmail.fr>
2023-02-06 10:22:50 +0100

    Fix background

--- frontend/src/components/Background.jsx
+++ frontend/src/components/Background.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function Background() {
   return (
     <div className="flex">
-      <div className="bg-[#1A1A1A] dark:bg-[#121212] flex justify-center items-center w-1/6 py-20 sm:py-24">
+      <div className="bg-cover flex justify-center items-center w-1/6 py-20 sm:py-24 dark:border-4 border-[#121212]">
         <h1 className="-rotate-90 text-[#FFFFFF] text-2xl sm:text-4xl font-extrabold text-center">
           DÉVELOPPEUR WEB
         </h1>
       </div>
-      <div className="bg-cover flex justify-center items-center w-5/6 ">
-        <h1 className="text-[#FFFFFF] text-4xl sm:text-7xl font-extrabold text-center">
+      <div className="flex dark:bg-[#313131] dark:text-[#FFFFFF] justify-center items-center w-5/6 ">
+        <h1 className="text-4xl sm:text-7xl font-extrabold text-center">
           JOY MARKARIAN
         </h1>
       </div>
